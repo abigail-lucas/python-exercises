01f2e53db2ff3f3a8bedfd74aa98d0b34305fce4
Abigail <abigailwolfmoon@gmail.com>
2020-09-21 11:02:20 +0200

    First day's work

---
+++ day1.py
@@ -0,0 +1,9 @@
+print("Hello world")
+
+class_name = "Class 2"
+
+print("I was coded in " + class_name)
+
+class_students = "19"
+
+print("There are " + class_students + " students in " + class_name)
